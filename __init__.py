c407d9576f1f3c1678fc08f5334a227b67fefcc5
xNykram <nykram@wp.pl>
2020-05-06 11:56:42 +0200

    changed way to connect to the database

--- __init__.py
+++ __init__.py
@@ -2,22 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, url_for, flash, redirect, request, session, jsonify
-from flaskext.mysql import MySQL
 import mcfuc
 import forms
-
+import mysql
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '4bf8ccad8c649d8cd98a0215663ecd97'
-app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = ""
-app.config['MYSQL_DATABASE_DB'] = "Users"
-app.config['MYSQL_DATABASE_HOST'] = "localhost"
 
-mysql = MySQL()
-mysql.init_app(app)
-
+mysqlModule = mysql.mysqlObj()
+mysqlModule.DataUpdater()
 
 @app.route("/login", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
@@ -27,24 +21,21 @@ def login():
         Fusername = form.username.data
         Fpassword = form.password.data
         try:
-            connection = mysql.connect()
-            cursor = connection.cursor()
-            cursor.execute("SELECT nickname from f_Users where nickname = '{}'".format(Fusername))
-            mysqlUsername = cursor.fetchone()
-            if mysqlUsername == None:
+            username = mysqlModule.Search("SELECT nickname from f_Users where nickname = '{}'".format(Fusername))
+            if username == None:
                 flash("Nie znaleziono uzytkownika o takim nicku: {}".format(Fusername), category='danger')
                 return redirect(url_for('login'))
-            cursor.execute("SELECT * from f_Users where passwd = MD5(CONCAT(salt, '{}'))".format(Fpassword))
-            mysqPassword = cursor.fetchone()
-            if mysqPassword == None:
+            password = mysqlModule.Search("SELECT * from f_Users where passwd = MD5(CONCAT(salt, '{}'))".format(Fpassword))
+            if password == None:
                 flash("Bledne haslo dla uzytkownika {}".format(Fusername), category='danger')
                 return redirect(url_for('login'))
-            connection.close()
-            session['username'] = mysqlUsername[0]
-            flash("Pomyslnie zalogowano jako {}".format(mysqlUsername[0]), category='success')
+            session['username'] = username[0]
+            mysqlModule.DataUpdater()
+            flash("Pomyslnie zalogowano jako {}".format(username[0]), category='success')
             return redirect(url_for('admin'))
         except Exception as e:
-            flash('Wystapil nieoczekiwany blad! -> {}'.format(e), category='warning')
+            print(e)
+            flash('Wystapil blad z serwerem, prosze sprobowac pozniej.', category='warning')
             return redirect(url_for('login'))
     return render_template('login.html', title='Logowanie', form=form)
 
@@ -53,21 +44,18 @@ def admin():
     form1 = forms.Run()
     form2 = forms.Close()
     form3 = forms.Console()
-    server_name = ''
     if 'username' in session:
-        mcmgr = mcfuc.mcManage(server_name , 25575, '')
+        mcmgr = mcfuc.mcManage(mysqlModule.server_ip, 25575, mysqlModule.server_password)
         if form1.submit1.data and form1.validate_on_submit():
-            print('Starting the server')
             print(mcmgr.runServer())
-            flash("Uruchamianie serwera", category="warning")
+            flash("Uruchamianie serwera, prosze czekac!", category="warning")
             return redirect(url_for("admin"))
         if form2.submit2.data and form2.validate_on_submit():
-            print('Closing the server')
-            print(mcmgr.closeServer())
-            flash("Serwer zostal wlaczony", category="success")
+            mcmgr.closeServer()
+            flash("Serwer zostanie wylaczony za chwile", category="success")
             return redirect(url_for("admin"))
         if form3.submit3.data and form3.validate_on_submit():
-            return redirect(url_for("console", server_name=server_name))
+            return redirect(url_for("console", server_name=mysqlModule.server_name))
         return render_template('admin.html', title='Panel administracyjny', form1=form1, form2=form2, form3=form3)
     else:
         return redirect(url_for('login'))
@@ -81,9 +69,8 @@ def logout():
 @app.route('/refresh', methods=['POST'])
 def refresh():
     if request.method == 'POST':
-            mcmgr = mcfuc.mcManage('', 25575, '')
+            mcmgr = mcfuc.mcManage(mysqlModule.server_ip, 25575, mysqlModule.server_password)
             mcmgr.stats()
-
             if mcmgr.status == True:
                 statusInfo = "Online"
                 color = 'badge-success'
@@ -96,11 +83,9 @@ def refresh():
 def refreshc():
     if request.method == 'POST':
        try:
-            mcmgr = mcfuc.mcManage('', 25575, '')
+            mcmgr = mcfuc.mcManage(mysqlModule.server_ip, 25575, mysqlModule.server_password)
             lastLog = mcmgr.downloadLogs()
-            lastlog = str(lastLog)
             replace = lastLog.replace("<"," ").replace(">", ":").replace("[m", " ").replace(""," ")
-            print(lastLog)
             return jsonify(log=replace)
        except Exception:
            pass
@@ -114,7 +99,7 @@ def console():
 def consoleForm():
     if request.method == "POST":
         command = request.form['data']
-        mcmgr = mcfuc.mcManage('', 25575, '')
+        mcmgr = mcfuc.mcManage(mysqlModule.server_ip, 25575, mysqlModule.server_password)
         mcmgr.command(command)
     return jsonify(response='Success')
 
